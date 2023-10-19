c06087bb4f816dc20a201bec389acb12d9dcca1b
Archana <kumariarchana98013@gmail.com>
2023-10-19 13:14:23 +0530

    todotask

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import "./App.css";
+import Header from "./Component/Header";
+import Form from "./Component/Form";
+import TodosList from "./Component/TodosList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const App=()=>{
+  const[input,setInput]=useState("");
+  const[todos,setTodos]=useState([]);
+  const[editTodo,setEditTodo]=useState(null);
+
+  return(
+    <div className="container">
+      <div className="app-wrapper">
+        <div>
+          <Header/>
+        </div>
+        <div>
+          <Form
+          input={input}
+          setInput={setInput}
+          todos={todos}
+          setTodos={setTodos}
+          editTodo={editTodo}
+          setEditTodo={setEditTodo}
+          />
+        </div>
+        <div>
+          <TodosList todos={todos} setTodos={setTodos} setEditTodo={setEditTodo}/>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
-
 export default App;
---
+++ src/Component/Header.js
@@ -0,0 +1,8 @@
+const Header=()=>{
+	return(
+		<div className="header">
+          <h1>Todo-List</h1>
+		</div>
+	)
+}
+export default Header;
